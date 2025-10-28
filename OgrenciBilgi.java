0687a5c94f2bfdd5f50c690db0f43029d2a0165c
Cihanak1 <250541107@firat.edu.tr>
2025-10-29 01:35:05 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,60 +1,56 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
- * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
- * 
- * Bu program kullanicidan ogrenci bilgilerini alir ve
- * duzenli bir formatta ekrana yazdirir.
- * Diğer java dosyalarının başında da bu örnek formattaki gibi kısa bilgi giriniz
+ * Ad Soyad: Cihan Akalın	
+ * Ogrenci No: 250541107	
+ * Tarih: 24 Ekim 2025		
+ * Aciklama: Ogrenci Bilgi Sistemi (Kullanicidan alinan veriyi formatli yazdirma kodu.)
  */
 
+
+package odev_2;
 import java.util.Scanner;
+import java.util.Spliterators.AbstractDoubleSpliterator;
+public class gorev1_1 {
 
-public class OgrenciBilgi {
-    public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
+	public static void main(String[] args) {
+		// Başlık kısmımız
+		System.out.println("=== OGRENCI BILGI SISTEMI ===");
+		
+		// 1. Scanner nesnesi oluşturuldu, klavyeden okumaya hazır.
+		Scanner input = new Scanner(System.in);
+		
+		// Doğru girdi alabilmek adına bilgilendirme çıktısı
+		System.out.print("Adinizi girin: ");
+		String ad = input.next();
+		
+		// Doğru girdi alabilmek adına bilgilendirme çıktısı
+		System.out.print("Soyadinizi girin: ");
+		String soyad = input.next();
+		
+		// Doğru girdi alabilmek adına bilgilendirme çıktısı
+		System.out.print("Ogrenci numaraniz: ");
+		int ogrenciNo = input.nextInt();
+		
+		// Doğru girdi alabilmek adına bilgilendirme çıktısı
+		System.out.println("Yasiniz: ");
+		int yas = input.nextInt();
+		
+		// Doğru girdi alabilmek adına bilgilendirme çıktısı
+		System.out.println("GPA (0.00-4.00): ");
+		double gpa = input.nextDouble();
+		final String Basarı = "Basarili Ogrenci";
+		
+		
+	    System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
+	    // %s -> String (metin)
+        // %d -> int (tam sayı)
+        // %.2f -> double - nokta sonrası 2 basamak göster.
+		System.out.printf("Ad Soyad: %s %s\n", ad,soyad );
+		System.out.printf("Öğrenci No: %d\n" , ogrenciNo );
+		System.out.printf("Yaş: %d\n", yas);
+		// GPA'nın 2 ondalık basamakla gösterilmesi zorunluluğu için %.2f kullandım.
+		System.out.printf("GPA: %.2f\n" , gpa);
+		// Sabit olarak tanımladığımız durumu yazdırıyorum.
+		System.out.printf("Durum: %s\n" , Basarı );
+	}
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
-    }
 }
